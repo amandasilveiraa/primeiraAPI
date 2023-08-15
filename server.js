7860648662e25c6e35842e5b454931711e8660fd
amandasilveiraa <amandasilveira.rs@gmail.com>
2023-08-15 11:36:50 -0300

    Primeira API

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3001, () => console.log('Running at port 3000') ) 
+//3000 = é a porta de comunicação entre o servidor e o computador
